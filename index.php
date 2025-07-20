031a5c879e4d1bef99b9e9c5f53df6d10e5b8649
maycon-douglas2005 <mayconchaves2005@gmail.com>
2025-07-20 17:25:12 -0300

    refactor: Cria nova branch
    Refatorando todo codigo de css puro para bootstrap

--- index.php
+++ index.php
@@ -5,44 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ingrid Chaves - Advocacia</title>
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/header.css">
     
+    <link rel="stylesheet" href="css/header.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
 
     <?php include('include/header.html') ?>
     
-    <main id="main"> 
+    <main id="container-fluid"> 
         
-        <div class="nomeEbio">
-            <h1>INGRID CAROLINE ALVES PEREIRA CHAVES</h1>
-            <p>
-                Advogada e bacharel em Direito pela 
-                FACULDADE FAPAN, atuante na área 
-                jurídica desde 2020 e com vasta 
-                experiência em Direito Trabalhista, 
-                Direito do Consumidor, Direito Civil, 
-                Direito de Família e Direito Criminal, inclusive 
-                em demandas extrajudiciais.  
-            </p>
-        </div>
+        <section class="row d-flex justify-content-center align-items-center">
+            <div class="col-6">
+                <section class="row nomeEbio ms-2 mt-2">
+                    <h1 class="fs-3">INGRID CAROLINE ALVES PEREIRA CHAVES</h1>
+                    <p>
+                        Advogada e bacharel em Direito pela 
+                        FACULDADE FAPAN, atuante na área 
+                        jurídica desde 2020 e com vasta 
+                        experiência em Direito Trabalhista, 
+                        Direito do Consumidor, Direito Civil, 
+                        Direito de Família e Direito Criminal, inclusive 
+                        em demandas extrajudiciais.  
+                    </p>
+                </section>
 
-        <div class="especialidades">
-            <h2>Especialidades:</h2>
+                <section class="row especialidades ms-2 mt-2">
+                    <h2>Especialidades:</h2>
 
-            <p>Atuação Estratégica no Direito do Trabalhador</p>
-            <p>Direito do Consumidor</p>
-            <p>Direito Civil</p>
-            <p>Direito de Família</p>
-            <p>Direito Criminal</p>
-        </div>
+                    <p>Atuação Estratégica no Direito do Trabalhador</p>
+                    <p>Direito do Consumidor</p>
+                    <p>Direito Civil</p>
+                    <p>Direito de Família</p>
+                    <p>Direito Criminal</p>
+                </section>
+            </div>
 
-        <img src="img/nina2.jpg" alt="Foto de perfil de Ingrid Chaves">
+            <div class="col-6">
+                <div class="row d-flex justify-content-center align-items-center">
+                    <img class="img-fluid col-7 mt-3 shadow-lg " src="img/nina2.jpg" alt="Foto de perfil de Ingrid Chaves">
 
-        <div class="botao"></div>
+                    <button class="botao col-auto button bg-secondary text-end">></button>
+                </div>
+                
+            </div>
+        </section>
+    
     </main>
     
     <script src="js/botaoIndex.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
