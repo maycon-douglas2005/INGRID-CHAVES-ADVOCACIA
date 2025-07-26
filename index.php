a0914a709f5d006e39f575a9115ae232f4dbacea
maycon-douglas2005 <mayconchaves2005@gmail.com>
2025-07-26 16:54:07 -0300

    refactor: final version of project without responsiveness
    remove pastas desnecessárias (css, js), conclui refatoração de codigo css para bootstrap e junta as duas páginas em uma só (na index.php), altera layout de cores.

--- index.php
+++ index.php
@@ -1,39 +1,107 @@
-    
-        
-        <section class="row d-flex justify-content-center align-items-center">
-            <div class="col-6">
-                <section class="row nomeEbio ms-2 mt-2">
-                    <h1 class="fs-3">INGRID CAROLINE ALVES PEREIRA CHAVES</h1>
-                    <p>
-                        Advogada e bacharel em Direito pela 
-                        FACULDADE FAPAN, atuante na área 
-                        jurídica desde 2020 e com vasta 
-                        experiência em Direito Trabalhista, 
-                        Direito do Consumidor, Direito Civil, 
-                        Direito de Família e Direito Criminal, inclusive 
-                        em demandas extrajudiciais.  
-                    </p>
-                </section>
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-                <section class="row especialidades ms-2 mt-2">
-                    <h2>Especialidades:</h2>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingrid Chaves - Advocacia</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
 
-                    <p>Atuação Estratégica no Direito do Trabalhador</p>
-                    <p>Direito do Consumidor</p>
-                    <p>Direito Civil</p>
-                    <p>Direito de Família</p>
-                    <p>Direito Criminal</p>
-                </section>
-            </div>
+<body>
+    <?php include('include/header.html'); ?>
 
-            <div class="col-6">
-                <div class="row d-flex justify-content-center align-items-center">
-                    <img class="img-fluid col-7 mt-3 shadow-lg " src="img/nina2.jpg" alt="Foto de perfil de Ingrid Chaves">
+    <main class="carousel slide container-fluid" id="carouselFather" data-bs-ride="carousel">
+
+        <div class="carousel-inner">
+
+            <section class="carousel-item active " data-bs-interval="8000">
+                <div class="content_slide_1 d-flex justify-content-center">
+
+                    <div class="col-6 d-flex flex-column justify-content-evenly">
+                        <div class="row nomeEbio ms-2 mt-2">
+                            <h1 class="fs-3">INGRID CAROLINE ALVES PEREIRA CHAVES</h1>
+                            <p class="fs-5">
+                                Advogada e bacharel em Direito pela
+                                FACULDADE FAPAN, atuante na área
+                                jurídica desde 2020 e com vasta
+                                experiência em Direito Trabalhista,
+                                Direito do Consumidor, Direito Civil,
+                                Direito de Família e Direito Criminal, inclusive
+                                em demandas extrajudiciais.
+                            </p>
+                        </div>
+
+                        <div class="row especialidades ms-2 mt-2">
+                            <h2>Especialidades:</h2>
+
+                            <ul class="list-unstyled">
+                                <li class="fs-5 ps-1 my-1 w-75" style="color: white; background-color: #2c2f36cf;">Atuação Estratégica no Direito do Trabalhador</li>
+                                <li class="fs-5 ps-1 my-1 w-50" style="color: white; background-color: #2c2f36cf;">Direito do Consumidor</li>
+                                <li class="fs-5 ps-1 my-1 w-25" style="color: white; background-color: #2c2f36cf;">Direito Civil</li>
+                                <li class="fs-5 ps-1 my-1 w-50" style="color: white; background-color: #2c2f36cf;">Direito de Família</li>
+                                <li class="fs-5 ps-1 my-1 w-25" style="color: white; background-color: #2c2f36cf;">Direito Criminal</li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="row d-flex justify-content-center align-items-center">
+                            <img class="img-fluid shadow-lg " style="height: 88.2dvh;" src="img/nina2.jpg" alt="Foto de perfil de Ingrid Chaves">
+
+                        </div>
+
+                    </div>
 
                 </div>
-                
-            </div>
-        </section>
 
-    
-    
+            </section>
+
+            <section class="carousel-item " data-bs-interval="8000">
+                
+
+                <div class="d-flex justify-content-center content_slide_2">
+                    <div class="col-4 d-flex justify-content-center">
+                        <img class="img-fluid shadow-lg" src="img/nina1.jpg" alt="Foto de Ingrid Chaves" style="height: 88.2dvh;">
+
+                    </div>
+                    
+
+                    <div class="col-6 d-flex flex-column justify-content-center">
+
+                        <h2 class="align-self-center">CONTATO</h2>
+                        
+                        <p class="col-auto fs-5 h2 ms-3 ps-1 pb-1" style="color: white; background-color: #2c2f36cf;">Tipo de Consultoria: Online e Presencial (com hora marcada)</p>
+                        <p class="col-auto fs-5 h2 ms-3 ps-1 w-75" style="color: white; background-color: #2c2f36cf;">Agende pelo WhatsApp: (11) 95833-7562 <a href="https://wa.me/5511958337562?text=Olá!%20Eu%20gostaria%20de%20realizar%20um%20agendamento." aria-label="Enviar uma mensagem no WhatsApp para Ingrid Chaves"><img style="width: 30px;" id="iconeWhatsApp" src="img/whats.png" alt="icone do aplicativo whatszapp"></a></p>
+                        <p class="col-auto fs-5 h2 ms-3 ps-1 w-75" style="color: white; background-color: #2c2f36cf;">Agende pelo email: ingridcaroline@gmail.com <a href="mailto:ingridcaroline@gmail.com?subject=Realizar%20Agendamento&body=Olá!%20Eu%20gostaria%20de%20realizar%20um%20agendamento..." aria-label="Enviar um E-mail para Ingrid Chaves"><img style="width: 30px;" src="img/email.png" id="iconeGmail" alt="icone do Gmail"></a></p>
+                        
+                    </div>
+
+
+                </div>
+            </section>
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselFather" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+               
+            </button>
+
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselFather" data-bs-slide="next">
+                <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+                
+            </button>
+        </div>
+    </main>
+
+
+
+
+    <!--  JAVASCRIPT  -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- js/botaoIndex.js
+++
@@ -1,7 +0,0 @@
-let botao = document.querySelector('.botao')
-
-function trocarTela() {
-   window.location.href = "view/contato.php"
-}
-
-botao.addEventListener('click', trocarTela)
